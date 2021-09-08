ecdf1063661ea13a7f133ca41e87bc1bae49df00
Adarsh999bh <adarshbhandary48@gmail.com>
2021-09-08 08:20:32 +0530

    welcome message added

---
+++ cards/src/main/java/com/deck/App.java
@@ -0,0 +1,13 @@
+package com.deck;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
